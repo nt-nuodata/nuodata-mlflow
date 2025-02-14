7ee0b7143563f243919524f7b4cc1786a4916839
Naveen-NucleusTeq <naveen.solanki@nucleusteq.com>
2025-02-14 14:52:06 +0530

    Added ECR Policy to Keep only 5 most recent images

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,3 +8,25 @@ resource "aws_ecr_repository" "application_ecr_repository" {
 
   tags = var.tags
 }
+
+resource "aws_ecr_lifecycle_policy" "ai_ml_studio_ecr_lifecycle_policy" {
+  repository = aws_ecr_repository.application_ecr_repository.name
+  policy     = <<POLICY
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Keep only 5 most recent images",
+      "selection": {
+        "tagStatus": "any",
+        "countType": "imageCountMoreThan",
+        "countNumber": 5
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+POLICY
+}
